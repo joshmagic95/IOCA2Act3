ad0a729767de4a8f6581e3965db5852fdef9d6e7
joshmagic95 <joshfarrell88@gmail.com>
2022-02-10 15:34:05 +0800

    Add project files.

---
+++ IOCA2Act3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Name: Joshua Farrell
+// ID: M153428
+// Date: 10 February 2022
+// Program descrption:
+// This program reads an input and tests if it is
+// greater than or less than a constant value (e.g., 10)
+// using a SWITCH-CASE statement
+
+namespace IOCA2Act2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            int inputValue;
+            bool inputOK;
+            const int constantValue = 10;
+
+            // Read an input from the user and test if it is an integer
+            Console.Write("Enter a number: ");
+            inputOK = int.TryParse(Console.ReadLine(), out inputValue);
+
+            // Using a SWITCH-CASE statement, test if the input value is greater than the constant value
+            switch (inputValue)
+            {
+                case 1;
+            }
+
+
+
+
+            //// Test if the input value is an integer
+            //if (inputOK)
+            //{
+            //    // Test if the input value is greater or less than the constant value
+            //    if (inputValue > constantValue)
+            //    {
+            //        Console.WriteLine("Your number {0} is greater than the constant {1}", inputValue, constantValue);
+            //    }// IF inputValue > constantValue
+            //    else if (inputValue < constantValue)
+            //    {
+            //        Console.WriteLine("Your number {0} is lesser than the constant {1}", inputValue, constantValue);
+            //    }// ELSE IF inputValue < constantValue
+            //    else
+            //    {
+            //        Console.WriteLine("Your number {0} is equal to the constant {1}", inputValue, constantValue);
+            //    }// ELSE inputValue
+            //}// IF inputOK
+            //else // If inputOK is false (inputValue is NOT an integer) then display an error
+            //{
+            //    Console.WriteLine("ERROR: Please enter an integer number.");
+            //}// ELSE inputOK
+        }
+    }
+}
